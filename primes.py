0d8f7c7bd82e1050f5d946c5ad2ebef51bd4496e
Tom Hurford <hurford.t0@gmail.com>
2022-10-01 16:23:55 +0100

    011022 - TGH - Assignment Complete

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def primes(number_of_primes):
-    list = []
-    return list
+# if n is 0 or negative return a ValueError
+
+def primes(n):
+    """Return a list of n prime numbers."""
+    if n <= 0:
+        raise ValueError("n must be a positive integer")
+    primes = []
+    i = 2
+    while len(primes) < n:
+        if is_prime(i):
+            primes.append(i)
+        i += 1
+    return primes
+
+def is_prime(n):
+    """Return True if n is prime."""
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
